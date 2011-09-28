85ab1df1fe7ae104c439f9f9c22b288874c07c55
Unbit <info@unbit.it>
2011-09-28 21:22:14 +0200

    fixed UNIX socket support

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from distutils.core import setup
 
 setup(name='uwsgitop',
-        version='0.1',
+        version='0.2.2',
         description='uWSGI top-like interface',
-        scripts=['uwsgitop'] 
+        scripts=['uwsgitop'],
+        install_requires = ['simplejson']
         )
