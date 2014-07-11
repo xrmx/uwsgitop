de7df9f17703ea8f7870684ce1f06bd962e49a2b
unbit <info@unbit.it>
2014-07-11 06:55:33 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from distutils.core import setup
 setup(name='uwsgitop',
         version='0.8',
         description='uWSGI top-like interface',
+        license='MIT',
         scripts=['uwsgitop'],
         install_requires = ['simplejson']
         )
