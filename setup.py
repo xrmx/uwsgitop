45630f78bdccad15ef541b7976f506978b6f0e43
Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>
2015-10-09 17:46:35 +0200

    Cleanup setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
-from distutils.core import setup
+from setuptools import setup
+import os
 
-setup(name='uwsgitop',
-        version='0.8',
-        description='uWSGI top-like interface',
-        license='MIT',
-        scripts=['uwsgitop'],
-        install_requires = ['simplejson']
-        )
+VERSION = '0.8'
+
+setup(
+    maintainer='Riccardo Magliocchetti',
+    maintainer_email='riccardo.magliocchetti@gmail.com',
+    name='uwsgitop',
+    version=VERSION,
+    description='uWSGI top-like interface',
+    license='MIT',
+    long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
+    scripts=['uwsgitop'],
+    install_requires = ['simplejson']
+)
