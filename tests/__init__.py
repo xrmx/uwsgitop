90eef92d0a372e96feeff3787beea64e63465627
Riccardo Magliocchetti <riccardo.magliocchetti@gmail.com>
2016-12-28 17:23:55 +0100

    WIP Debug prints

--- tests/__init__.py
+++ tests/__init__.py
@@ -10,6 +10,7 @@ UWSGI =  os.environ.get('UWSGI_BIN', 'uwsgi')
 
 class TestUwsgitop(unittest.TestCase):
     def setUp(self):
+        print("SETUPO")
         self.stdin = open(os.devnull, 'r')
         self.stdout = open(os.devnull, 'w')
         self.stderr = tempfile.TemporaryFile()
@@ -20,7 +21,9 @@ class TestUwsgitop(unittest.TestCase):
         self.stdin.close()
         self.stdout.close()
         self.stderr.close()
+        print("TEARDOWN BEFORE TERMINATE")
         self.terminate_cmd(self.commands)
+        print("TEARDOWN END")
 
     def run_cmd(self, command, wait=0):
         cmd = subprocess.Popen(
@@ -36,12 +39,17 @@ class TestUwsgitop(unittest.TestCase):
         return cmd
 
     def terminate_cmd(self, commands):
+        print("BEFORE TERMINATE")
+        print(commands)
         for cmd in commands:
             cmd.terminate()
+        print("AFTER TERMINATE")
         for cmd in commands:
-            cmd.wait()
+            pass
+            #cmd.wait()
 
     def test_read_from_unix_socket(self):
+        print("AAAAAAAAAAA")
         uwsgi = self.run_cmd(
             [UWSGI, '--http', ':0', '--stats', '/tmp/stats.socket', '--logto', '/dev/null']
         )
@@ -49,6 +57,7 @@ class TestUwsgitop(unittest.TestCase):
         self.assertEqual(self.stderr.tell(), 0)
 
     def test_read_from_socket(self):
+        print("AAAAAAAAAAA")
         uwsgi = self.run_cmd(
             [UWSGI, '--http', ':0', '--stats', ':3031', '--logto', '/dev/null']
         )
@@ -56,6 +65,7 @@ class TestUwsgitop(unittest.TestCase):
         self.assertEqual(self.stderr.tell(), 0)
 
     def test_read_from_http(self):
+        print("AAAAAAAAAAA")
         uwsgi = self.run_cmd(
             [UWSGI, '--http', ':0', '--stats', ':3031', '--stats-http', '--logto', '/dev/null']
         )
