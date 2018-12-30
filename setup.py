844d9cc7f04ae1a40b54d60cb9d8ad884a8673e0
Loren Carvalho <lcarvalho@linkedin.com>
2018-12-30 15:28:50 +0100

    setup: make argparse mandatory only for python 2

--- setup.py
+++ setup.py
@@ -12,8 +12,8 @@ setup(
     license='MIT',
     long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
     scripts=['uwsgitop'],
-    install_requires = [
-        'argparse',
+    install_requires=[
+        'argparse;python_version<"3"',
     ],
     classifiers=[
         'Programming Language :: Python',
